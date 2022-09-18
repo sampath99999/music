c7d3df65e8e706d20eaf68e8a1f51636e94d1724
Sampath Bandla <sampathbandla@lewisu.edu>
2022-09-18 22:23:13 +0530

    Admin Login page done!

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get("/dashboard",(req,res) => {
-    res.render("admin-dashboard")
+router.get("/",(req,res) => {
+    res.render("admin/index.ejs")
 })
 
 
